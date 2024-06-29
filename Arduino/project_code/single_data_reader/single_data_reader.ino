9b924103aaaa2a9aa5ea25406c24bd0ab4a717a8
KetAveryH <khollingsworth@g.hmc.edu>
2024-06-28 21:01:53 -0700

    Arduino single_data_reader two Device Update + ESP_IDF gpio_example Code

--- Arduino/project_code/single_data_reader/single_data_reader.ino
+++ Arduino/project_code/single_data_reader/single_data_reader.ino
@@ -8,116 +8,107 @@
 #define SD_MMC_D0  40 //Please do not modify it.
 
 // Define GPIO pins
-#define DEVICE1_SIGNAL_PIN 42
-#define DEVICE1_ACK_PIN 41
-#define DEVICE2_RECEIVE_PIN 1
-#define DEVICE2_ACK_PIN 2
+#define DEVICE_SIGNAL_PIN1 42
+#define DEVICE_SIGNAL_PIN2 41
 
-volatile bool device1Sending = false;
-volatile bool device2Receiving = false;
+Adafruit_INA219 ina219(0x40);
 
-int signalState = 0;
+
+int signal1Received = 0;
+int signal2Received = 0;
+
 int initialMillis = 0;
 
-HardwareSerial SerialPort(1); // Creates an instance for UART1
-
-// Sets acknowledge pin to high on rising edge of Signal Pin
-// This signifies the other ESP to set its signal pin to low
-void IRAM_ATTR ackPinHighISR() {
-    signalState = 1;
-    digitalWrite(DEVICE1_ACK_PIN, HIGH);
-    Serial.println("Acknowledgement line set High");
+// On falling edge of signal, signalReceived variables are set to 1
+void IRAM_ATTR signal1ISR() {
+  signal1Received = 1;
 }
-
-// Sets acknowledge pin back to low on falling edge of signal pin/
-// Resets the state of both lines
-void IRAM_ATTR ackPinLOWISR() {
-    digitalWrite(DEVICE1_ACK_PIN, LOW);
-    Serial.println("Acknowledgement line set Low");
+void IRAM_ATTR signal2ISR() {
+  signal2Received = 1;
 }
 
 
 void setup() {
-    SerialPort.begin(115200, SERIAL_8N1, 47, 21); // Initializes UART1 with custom TX (GPIO10) and RX (GPIO9) pins
-    // SD_MMC INITIALIZATION
-
-    pinMode(DEVICE1_SIGNAL_PIN, INPUT);
-    pinMode(DEVICE1_ACK_PIN, OUTPUT);
+    Serial.begin(115200);
+    
+    pinMode(DEVICE_SIGNAL_PIN1, INPUT);
+    pinMode(DEVICE_SIGNAL_PIN2, INPUT);
 
     SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
     if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
-      Serial.println("Card Mount Failed");
-      return;
+        Serial.println("Card Mount Failed");
+        return;
     }
+    
     uint8_t cardType = SD_MMC.cardType();
-    if(cardType == CARD_NONE){
+    if (cardType == CARD_NONE) {
         Serial.println("No SD_MMC card attached");
         return;
     }
 
     uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
     Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
-
-        
-
+    Wire.begin(16, 17);
     listDir(SD_MMC, "/", 0);
 
-    // INA 219 INITIALIZATION
-
-    // Open serial communications and wait for port to open:
-    Wire.begin(16, 17);
-    Serial.begin(115200);
-    while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB port only
-    }
-
-    if (! ina219.begin()) {
+    if (!ina219.begin()) {
         Serial.println("Failed to find INA219 chip");
     }
 
     deleteFile(SD_MMC, "/data.csv"); // TEMPORARY
-    writeFile(SD_MMC, "/data.csv", "Time, Bus Voltage, Shunt Voltage, Load Voltage, Current_mA, Power_mA, Events, dataReceived\n");
-    // on rising edge of signal pin, interrupt and set acknowledge pin to high
-    attachInterrupt(digitalPinToInterrupt(DEVICE1_SIGNAL_PIN), ackPinHighISR, RISING);
-    // attachInterrupt(digitalPinToInterrupt(DEVICE2_SIGNAL_PIN), ackPinHighISR, RISING);
-    // on falling edge of signal pin, interrupt and set acknowledge pin to low, re-establishing initial conditions
-    attatchInterrupt(digitalPinToInterrupt(DEVICE1_SIGNAL_PIN), ackPinLowISR, FALLING);
-    // attatchInterrupt(digitalPinToInterrupt(DEVICE2_SIGNAL_PIN), ackPinLowISR, FALLING);
+    writeFile(SD_MMC, "/data.csv", "Time, Bus Voltage, Shunt Voltage, Load Voltage, Current_mA, Power_mA, Signal1, Signal2 \n");
+
+    attachInterrupt(digitalPinToInterrupt(DEVICE_SIGNAL_PIN1), signal1ISR, FALLING);
+    attachInterrupt(digitalPinToInterrupt(DEVICE_SIGNAL_PIN2), signal2ISR, FALLING);
     initialMillis = millis();
 }
 
 void loop() {
-    signalState = digitalRead(signalPin);
-    Serial.print(signalState);
 
-    
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+  // Serial.print(power_mW);
+  int curTime = millis() - initialMillis;
 
-    float shuntvoltage = 0;
-    float busvoltage = 0;
-    float current_mA = 0;
-    float loadvoltage = 0;
-    float power_mW = 0;
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-    int curMillis = millis() - initialMillis;
+  String row = String(curTime) + ", " +
+          String(busvoltage) + ", " + 
+          String(shuntvoltage) + ", " + 
+          String(loadvoltage) + ", " + 
+          String(current_mA) + ", " + 
+          String(power_mW) + ", " +
+          String(signal1Received) + ", " +
+          String(signal2Received) + "\n";
 
-    shuntvoltage = ina219.getShuntVoltage_mV();
-    busvoltage = ina219.getBusVoltage_V();
-    current_mA = ina219.getCurrent_mA();
-    power_mW = ina219.getPower_mW();
-    loadvoltage = busvoltage + (shuntvoltage / 1000);
+  const char* row_c_str = row.c_str();
+  // Serial.println(row_c_str);
+  appendFile(SD_MMC, "/data.csv", row_c_str);
+  // Serial.println(digitalRead(DEVICE_SIGNAL_PIN));
 
-    String row = currentTime + ", " +
-            String(busvoltage) + ", " + 
-            String(shuntvoltage) + ", " + 
-            String(loadvoltage) + ", " + 
-            String(current_mA) + ", " + 
-            String(power_mW) + ", " +
-            String(signalState) + "\n";
+  if (signal1Received == 1) {
+    // Serial.println("Signal Received 1");
+    if (digitalRead(DEVICE_SIGNAL_PIN1) == 1) {
+      signal1Received = 0;
+      Serial.println("Line 1 reset");
+    }
+  }
 
-    // Write to SD
-    const char* row_c_str = row.c_str();
-    
-    appendFile(SD_MMC, "/data.csv", row_c_str);
+  if (signal2Received == 1) {
+    // Serial.println("Signal Received 2");
+    if (digitalRead(DEVICE_SIGNAL_PIN2) == 1) {
+      signal2Received = 0;
 
-    delay(1000);
+      Serial.println("Line 2 reset");
+    }
+  }
+
+  delay(5);
 }
