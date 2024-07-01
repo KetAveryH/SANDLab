3b23a5ac0c597f76459119b3a8d6fbedd75f4f86
KetAveryH <khollingsworth@g.hmc.edu>
2024-07-01 12:01:35 -0700

    Scribe accurately reports power and signal drops; bug in differencing

--- Arduino/project_code/scribe/scribe.ino
+++ Arduino/project_code/scribe/scribe.ino
@@ -22,14 +22,17 @@
 
 // Each of these represents an edge node performing inference. 
 // Uncomment the edge nodes you will be using
+#define NUM_NODES 6
 #define NODE_1
 #define NODE_2
-#define NODE_3
-#define NODE_4
-#define NODE_5
-#define NODE_6
+// #define NODE_3
+// #define NODE_4
+// #define NODE_5
+// #define NODE_6
 
 int initialMillis = 0;
+int refMillis = 0;
+int millisDif = 0;
 
 
 // INA219's have a max of 4 distinguishable addresses per SDA and SCL pair (I2C line)
@@ -40,7 +43,15 @@ int initialMillis = 0;
     Adafruit_INA219 ina219_1_1(INA_ADDRESS_1_1);
     int signal_received_1 = 0;
     void IRAM_ATTR signal1ISR() {  // ISR function ensures that on falling edge of the signal pin, the data is recorded
-    signal_received_1 = 1;
+        signal_received_1 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_1 = "N/A";
@@ -51,8 +62,17 @@ int initialMillis = 0;
     #define INA_ADDRESS_1_2 0x41
     Adafruit_INA219 ina219_1_2(INA_ADDRESS_1_2);
     int signal_received_2 = 0;
+
     void IRAM_ATTR signal2ISR() {
-    signal_received_2 = 1;
+        signal_received_2 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_2 = "N/A";
@@ -64,7 +84,15 @@ int initialMillis = 0;
     Adafruit_INA219 ina219_1_3(INA_ADDRESS_1_3);
     int signal_received_3 = 0;
     void IRAM_ATTR signal3ISR() {
-    signal_received_3 = 1;
+        signal_received_3 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_3 = "N/A";
@@ -76,7 +104,15 @@ int initialMillis = 0;
     Adafruit_INA219 ina219_1_4(INA_ADDRESS_1_4);
     int signal_received_4 = 0;
     void IRAM_ATTR signal4ISR() {
-    signal_received_4 = 1;
+        signal_received_4 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_4 = "N/A";
@@ -89,7 +125,15 @@ int initialMillis = 0;
     Adafruit_INA219 ina219_2_5(INA_ADDRESS_2_5);
     int signal_received_5 = 0;
     void IRAM_ATTR signal5ISR() {
-    signal_received_5 = 1;
+        signal_received_5 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_5 = "N/A";
@@ -101,7 +145,15 @@ int initialMillis = 0;
     Adafruit_INA219 ina219_2_6(INA_ADDRESS_2_6);
     int signal_received_6 = 0;
     void IRAM_ATTR signal6ISR() {
-    signal_received_6 = 1;
+        signal_received_6 = 1;
+
+        // Return the difference in milliseconds
+        if (refMillis > 0) {
+            millisDif = millis() - refMillis;
+            refMillis = 0;
+        } else { 
+            refMillis = millis(); // If signal is for sending record start time
+        }
     }
 #else
     const char* signal_received_6 = "N/A";
@@ -121,6 +173,7 @@ void reset_signal_variables() {
     #ifdef NODE_1
     if (signal_received_1 == 1 && digitalRead(SIGNAL_PIN_1) == 1) {
         signal_received_1 = 0;
+        initialMillis = 0;
         Serial.println("Line 1 reset");
     }
     #endif
@@ -161,10 +214,87 @@ void reset_signal_variables() {
     #endif
 }
 
+String get_ina_data() {
+    float power_data[NUM_NODES * 5];  // Initialize the power_data array
+    int curTime = millis() - initialMillis;
+
+    int index = 0;  // Index to track position in the power_data array
+
+    #ifdef NODE_1
+    power_data[index++] = ina219_1_1.getShuntVoltage_mV();
+    power_data[index++] = ina219_1_1.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_1_1.getCurrent_mA();
+    power_data[index++] = ina219_1_1.getPower_mW();
+    #endif
+
+    #ifdef NODE_2
+    power_data[index++] = ina219_1_2.getShuntVoltage_mV();
+    power_data[index++] = ina219_1_2.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_1_2.getCurrent_mA();
+    power_data[index++] = ina219_1_2.getPower_mW();
+    #endif
+
+    #ifdef NODE_3
+    power_data[index++] = ina219_1_3.getShuntVoltage_mV();
+    power_data[index++] = ina219_1_3.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_1_3.getCurrent_mA();
+    power_data[index++] = ina219_1_3.getPower_mW();
+    #endif
+
+    #ifdef NODE_4
+    power_data[index++] = ina219_1_4.getShuntVoltage_mV();
+    power_data[index++] = ina219_1_4.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_1_4.getCurrent_mA();
+    power_data[index++] = ina219_1_4.getPower_mW();
+    #endif
+
+    #ifdef NODE_5
+    power_data[index++] = ina219_2_5.getShuntVoltage_mV();
+    power_data[index++] = ina219_2_5.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_2_5.getCurrent_mA();
+    power_data[index++] = ina219_2_5.getPower_mW();
+    #endif
+
+    #ifdef NODE_6
+    power_data[index++] = ina219_2_6.getShuntVoltage_mV();
+    power_data[index++] = ina219_2_6.getBusVoltage_V();
+    power_data[index++] = power_data[index - 1] + (power_data[index - 3] / 1000); // Load voltage = bus voltage + (shunt voltage / 1000)
+    power_data[index++] = ina219_2_6.getCurrent_mA();
+    power_data[index++] = ina219_2_6.getPower_mW();
+    #endif
+
+    String row = String(curTime);
+    row += ", " + String(signal_received_1) + ", " +
+                String(signal_received_2) + ", " +
+                String(signal_received_3) + ", " +
+                String(signal_received_4) + ", " +
+                String(signal_received_5) + ", " +
+                String(signal_received_6) + ", " +
+                String(millisDif);
+    for (int i = 0; i < NUM_NODES * 5; i++) {
+        row += ", " + String(power_data[i]);
+    }
+    row += "\n";
+    
+    // Serial.println(row);
+
+    if (millisDif > 0) {
+        millisDif = 0;
+    } 
+
+    return row;
+}
+
 
 
 void setup() {
     Serial.begin(115200);
+    Wire.begin(47, 21); // SDA 47, SCL 21
 
     // Define Signal Pins and attach interrupts on their falling edges, interpret as a signal received.
 
@@ -214,17 +344,52 @@ void setup() {
 
     uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
     Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
-    Wire.begin(16, 17);
+    
     listDir(SD_MMC, "/", 0);
 
-    if (!ina219.begin()) {
-        Serial.println("Failed to find INA219 chip");
+
+    // Initialize each INA219 sensor
+    #ifdef NODE_1
+    if (!ina219_1_1.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x40 I2C1");
     }
+    #endif
+
+    #ifdef NODE_2
+    if (!ina219_1_2.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x41 I2C1");
+    }
+    #endif
+
+    #ifdef NODE_3
+    if (!ina219_1_3.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x44 I2C1");
+    }
+    #endif
+
+    #ifdef NODE_4
+    if (!ina219_1_4.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x45 I2C1");
+    }
+    #endif
+
+    #ifdef NODE_5
+    if (!ina219_2_5.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x40 I2C2");
+    }
+    #endif
+
+    #ifdef NODE_6
+    if (!ina219_2_6.begin()) {
+        Serial.println("Failed to find INA219 chip at address 0x41 I2C2z");
+    }
+    #endif
+
 
     // Wipe SD card for new data
 
     deleteFile(SD_MMC, "/data.csv"); // TEMPORARY
-    writeFile(SD_MMC, "/data.csv", "Time, Bus Voltage, Shunt Voltage, Load Voltage, Current_mA, Power_mA, Signal1, Signal2, Signal3, Signal4, Signal5, Signal6 \n");
+    writeFile(SD_MMC, "/data.csv", "Time, Signal1, Signal2, Signal3, Signal4, Signal5, Signal6, Latency, Bus Voltage 1, Shunt Voltage 1, Load Voltage 1, Current_mA 1, Power_mW 1, Bus Voltage 2, Shunt Voltage 2, Load Voltage 2, Current_mA 2, Power_mW 2, Bus Voltage 3, Shunt Voltage 3, Load Voltage 3, Current_mA 3, Power_mW 3, Bus Voltage 4, Shunt Voltage 4, Load Voltage 4, Current_mA 4, Power_mW 4, Bus Voltage 5, Shunt Voltage 5, Load Voltage 5, Current_mA 5, Power_mW 5, Bus Voltage 6, Shunt Voltage 6, Load Voltage 6, Current_mA 6, Power_mW 6\n");
 
 
     initialMillis = millis();
@@ -232,39 +397,11 @@ void setup() {
 
 
 void loop() {
-    float shuntvoltage = 0;
-    float busvoltage = 0;
-    float current_mA = 0;
-    float loadvoltage = 0;
-    float power_mW = 0;
-    // Serial.print(power_mW);
-    int curTime = millis() - initialMillis;
-
-    shuntvoltage = ina219.getShuntVoltage_mV();
-    busvoltage = ina219.getBusVoltage_V();
-    current_mA = ina219.getCurrent_mA();
-    power_mW = ina219.getPower_mW();
-    loadvoltage = busvoltage + (shuntvoltage / 1000);
-
-    String row = String(curTime) + ", " +
-            String(busvoltage) + ", " + 
-            String(shuntvoltage) + ", " + 
-            String(loadvoltage) + ", " + 
-            String(current_mA) + ", " + 
-            String(power_mW) + ", " +
-            String(signal_received_1) + ", " +
-            String(signal_received_2) + ", " +
-            String(signal_received_3) + ", " +
-            String(signal_received_4) + ", " +
-            String(signal_received_5) + ", " +
-            String(signal_received_6) + "\n";
-
+    String row = get_ina_data();
     const char* row_c_str = row.c_str();
-    // Serial.println(row_c_str);
     appendFile(SD_MMC, "/data.csv", row_c_str);
-    // Serial.println(digitalRead(DEVICE_SIGNAL_PIN));
 
     reset_signal_variables();
 
-    delay(5);
+    delay(10);
 }
